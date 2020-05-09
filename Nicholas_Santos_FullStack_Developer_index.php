5a8dbf082992617b022662d88a49487136c73da6
Nicholas Santos <55286153+nicksantoscs@users.noreply.github.com>
2020-05-08 20:25:42 -0400

    Initial commit

---
+++ Nicholas_Santos_FullStack_Developer_index.php
@@ -0,0 +1,15 @@
+<?php
+// set page title
+$title = 'Welcome';
+require_once ('Nicholas_Santos_FullStack_Developer_header.php');
+?>
+
+<h1>Prepr Full-Stack Developer Challenge</h1>
+
+<p>This is my submission for the Full-Stack Developer Challenge.</p>
+<p>Source code available<a href="insert repo link here">here</a>
+</p>
+
+<?php
+require_once 'Nicholas_Santos_FullStack_Developer_footer.php';
+?>
