d628d5c8dded99a10beb108f746500df7e01ab9b
Nicholas Santos <55286153+nicksantoscs@users.noreply.github.com>
2020-05-09 20:16:53 -0400

    Updated AWS MySQL server info

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 // Connect to AWS MySQL server
-$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', 'x');
+$db = new PDO('mysql:host=172.31.22.43;dbname=Nicholas_A1117292', 'Nicholas_A1117292', 'grnUGrRUw4');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
